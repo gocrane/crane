82a9d7c78f2aedc6dc9114698306845f7701256c
yuyufei <yufeiyu@tencent.com>
2022-03-20 21:01:20 +0800

    m3

--- pkg/server/handler/prediction/prediction.go
+++ pkg/server/handler/prediction/prediction.go
@@ -55,6 +55,7 @@ func NewDebugHandler(ctx context.Context) *DebugHandler {
 func (dh *DebugHandler) Display(c *gin.Context) {
 	namespace := c.Param("namespace")
 	name := c.Param("tsp")
+	klog.Infof("WWWW Display %s/%s.", namespace, name)
 
 	if len(namespace) == 0 || len(name) == 0 {
 		c.Writer.WriteHeader(http.StatusBadRequest)
@@ -66,6 +67,7 @@ func (dh *DebugHandler) Display(c *gin.Context) {
 		ginwrapper.WriteResponse(c, err, nil)
 		return
 	}
+	klog.Infof("WWWW Got TimeSeriesPrediction %s/%s.", tsp.Namespace, tsp.Name)
 
 	if len(tsp.Spec.PredictionMetrics) > 0 {
 		if tsp.Spec.PredictionMetrics[0].Algorithm.AlgorithmType == v1alpha1.AlgorithmTypeDSP && tsp.Spec.PredictionMetrics[0].Algorithm.DSP != nil  {
@@ -78,7 +80,7 @@ func (dh *DebugHandler) Display(c *gin.Context) {
 			internalConf := mc.ConvertApiMetric2InternalConfig(&tsp.Spec.PredictionMetrics[0])
 			namer := mc.GetMetricNamer(&tsp.Spec.PredictionMetrics[0])
 			predictor := interface{}(dh.predictorManager.GetPredictor(v1alpha1.AlgorithmTypeDSP)).(prediction.GenericPrediction)
-
+klog.Infof("WWWWWW aaaaa")
 			history, test, estimate, err := dsp.Debug(predictor, namer, internalConf)
 			if err != nil {
 				ginwrapper.WriteResponse(c, err, nil)
