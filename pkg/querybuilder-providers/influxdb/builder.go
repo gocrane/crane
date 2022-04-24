1e821002a2ad9c53b5a0f7ad8a5d56bde0f0251d
jasonzhu <491537461q@gmail.com>
2022-04-25 03:22:59 +0800

    fix: add todo for influxdb query builder.

--- pkg/querybuilder-providers/influxdb/builder.go
+++ pkg/querybuilder-providers/influxdb/builder.go
@@ -9,27 +9,28 @@ import (
 	"strings"
 )
 
+// TODO implement influxDB metrics semantic
 const (
 	// WorkloadCpuUsageExprTemplate is used to query workload cpu usage by promql,  param is namespace,workload-name,duration str
-	WorkloadCpuUsageExprTemplate = `sum(irate(container_cpu_usage_seconds_total{container!="",image!="",container!="POD",namespace="%s",pod=~"^%s-.*$"}[%s]))`
+	WorkloadCpuUsageExprTemplate = `from(bucket:"my-bucket")|> range(start: -%sh) |> aggregate.rate() |> cumulativeSum()`
 	// WorkloadMemUsageExprTemplate is used to query workload mem usage by promql, param is namespace, workload-name
-	WorkloadMemUsageExprTemplate = `sum(container_memory_working_set_bytes{container!="",image!="",container!="POD",namespace="%s",pod=~"^%s-.*$"})`
+	WorkloadMemUsageExprTemplate = ``
 
 	// following is node exporter metric for node cpu/memory usage
 	// NodeCpuUsageExprTemplate is used to query node cpu usage by promql,  param is node name which prometheus scrape, duration str
-	NodeCpuUsageExprTemplate = `sum(count(node_cpu_seconds_total{mode="idle",instance=~"(%s)(:\\d+)?"}) by (mode, cpu)) - sum(irate(node_cpu_seconds_total{mode="idle",instance=~"(%s)(:\\d+)?"}[%s]))`
+	NodeCpuUsageExprTemplate = ``
 	// NodeMemUsageExprTemplate is used to query node cpu memory by promql,  param is node name, node name which prometheus scrape
-	NodeMemUsageExprTemplate = `sum(node_memory_MemTotal_bytes{instance=~"(%s)(:\\d+)?"} - node_memory_MemAvailable_bytes{instance=~"(%s)(:\\d+)?"})`
+	NodeMemUsageExprTemplate = ``
 
 	// PodCpuUsageExprTemplate is used to query pod cpu usage by promql,  param is namespace,pod, duration str
-	PodCpuUsageExprTemplate = `sum(irate(container_cpu_usage_seconds_total{container!="POD",namespace="%s",pod="%s"}[%s]))`
+	PodCpuUsageExprTemplate = ``
 	// PodMemUsageExprTemplate is used to query pod cpu usage by promql,  param is namespace,pod
-	PodMemUsageExprTemplate = `sum(container_memory_working_set_bytes{container!="POD",namespace="%s",pod="%s"})`
+	PodMemUsageExprTemplate = ``
 
 	// ContainerCpuUsageExprTemplate is used to query container cpu usage by promql,  param is namespace,pod,container duration str
-	ContainerCpuUsageExprTemplate = `irate(container_cpu_usage_seconds_total{container!="POD",namespace="%s",pod=~"^%s.*$",container="%s"}[%s])`
+	ContainerCpuUsageExprTemplate = ``
 	// ContainerMemUsageExprTemplate is used to query container cpu usage by promql,  param is namespace,pod,container
-	ContainerMemUsageExprTemplate = `container_memory_working_set_bytes{container!="POD",namespace="%s",pod=~"^%s.*$",container="%s"}`
+	ContainerMemUsageExprTemplate = ``
 )
 
 var supportedResources = sets.NewString(v1.ResourceCPU.String(), v1.ResourceMemory.String())
