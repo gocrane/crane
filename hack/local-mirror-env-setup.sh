393e4a6b2c0b48d250040e6addd4956473252a6e
jun <2456868764@qq.com>
2023-02-12 14:11:35 +0800

    add local env setup from mirror helm repo

---
+++ hack/local-mirror-env-setup.sh
@@ -0,0 +1,46 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+CRANE_KUBECONFIG="${HOME}/.kube/config_crane"
+CRANE_CLUSTER_NAME="crane"
+
+echo "Step1: Create local cluster: " ${CRANE_KUBECONFIG}
+kind delete cluster --name="${CRANE_CLUSTER_NAME}" 2>&1
+kind create cluster --kubeconfig "${CRANE_KUBECONFIG}" --name "${CRANE_CLUSTER_NAME}" --image kindest/node:v1.21.1
+export KUBECONFIG="${CRANE_KUBECONFIG}"
+echo "Step1: Create local cluster finished."
+
+echo "Step2: Installing Prometheus "
+helm repo add prometheus-community-mirror https://finops-helm.pkg.coding.net/gocrane/prometheus-community
+helm install prometheus -n crane-system \
+                        --set prometheus-pushgateway.enabled=false \
+                        --set alertmanager.enabled=false \
+                        --set server.persistentVolume.enabled=false \
+                        -f https://gitee.com/finops/helm-charts/raw/main/integration/prometheus/override_values.yaml \
+                        --create-namespace  prometheus-community-mirror/prometheus
+echo "Step2: Installing Prometheus finished."
+
+echo "Step3: Installing Grafana "
+helm repo add grafana-mirror https://finops-helm.pkg.coding.net/gocrane/grafana
+helm install grafana \
+             -f https://gitee.com/finops/helm-charts/raw/main/integration/grafana/override_values.yaml \
+             -n crane-system \
+             --create-namespace grafana-mirror/grafana
+echo "Step3: Installing Grafana finished."
+
+echo "Step4: Installing Crane "
+helm repo add crane https://gocrane.github.io/helm-charts
+helm repo update
+helm install crane -n crane-system --set craneAgent.enable=false --create-namespace crane/crane
+helm install fadvisor -n crane-system --create-namespace crane/fadvisor
+echo "Step4: Installing Crane finished."
+
+kubectl get deploy -n crane-system
+echo "Please wait for all pods ready"
+echo "After all pods ready, Get the Crane Dashboard URL to visit by running these commands in the same shell:"
+echo "    export KUBECONFIG=${HOME}/.kube/config_crane"
+echo "    kubectl -n crane-system port-forward service/craned 9090:9090"
+
