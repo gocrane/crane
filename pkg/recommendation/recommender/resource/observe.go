661c85fb0cde97575dffb78f53fc2a28ce0d2c99
qmhu <brillantroad@gmail.com>
2023-02-20 19:57:32 +0800

    remove owner_replicas in metric

--- pkg/recommendation/recommender/resource/observe.go
+++ pkg/recommendation/recommender/resource/observe.go
@@ -1,7 +1,6 @@
 package resource
 
 import (
-	"context"
 	"encoding/json"
 	"fmt"
 
@@ -11,7 +10,6 @@ import (
 
 	"github.com/gocrane/crane/pkg/metrics"
 	"github.com/gocrane/crane/pkg/recommendation/framework"
-	"github.com/gocrane/crane/pkg/utils"
 )
 
 // Observe enhance the observability.
@@ -56,12 +54,6 @@ func (rr *ResourceRecommender) recordResourceRecommendation(ctx *framework.Recom
 		"resource":   resName.String(),
 	}
 
-	scale, _, err := utils.GetScaleFromObjectReference(context.TODO(), ctx.RestMapper, ctx.ScaleClient, ctx.Recommendation.Spec.TargetRef)
-	if err != nil {
-		return err
-	}
-	labels["owner_replicas"] = fmt.Sprintf("%d", scale.Spec.Replicas)
-
 	switch resName {
 	case v1.ResourceCPU:
 		metrics.ResourceRecommendation.With(labels).Set(float64(quantity.MilliValue()) / 1000.)
@@ -71,3 +63,4 @@ func (rr *ResourceRecommender) recordResourceRecommendation(ctx *framework.Recom
 
 	return nil
 }
+
