489680b95bbf5228a80b7d3569b2c1dc666c07b4
wentang <v_wentang@webank.com>
2023-02-21 11:12:31 +0800

    list recommendation add filter_options

--- pkg/web/src/pages/Recommend/ReplicaRecommend/components/SearchForm.tsx
+++ pkg/web/src/pages/Recommend/ReplicaRecommend/components/SearchForm.tsx
@@ -1,21 +1,31 @@
 import React, { memo } from 'react';
-import { Button, Col, Form, Input, Row, Select } from 'tdesign-react';
+import { Button, Col, Form, Input, Row, Select ,MinusCircleIcon} from 'tdesign-react';
 import _ from 'lodash';
 import { useTranslation } from 'react-i18next';
 
-const { FormItem } = Form;
+const { FormItem ,FormList} = Form;
 
 export type SearchFormProps = {
   recommendation: any;
   setFilterParams: any;
 };
-
 const SearchForm: React.FC<SearchFormProps> = ({ recommendation, setFilterParams }) => {
   const { t } = useTranslation();
   const onValuesChange = (changeValues: any, allValues: any) => {
     if (!allValues.name) delete allValues.name;
     if (!allValues.namespace) delete allValues.namespace;
     if (!allValues.workloadType) delete allValues.workloadType;
+    let filter_options: any=[];
+    if(allValues.filter_options_list.length){
+      allValues.filter_options_list.map((item: any)=>{
+        if(item.resource&&item.quotaType&& item.compareExpr &&item.ratio){
+          filter_options.push(`${item.resource},${item.quotaType},${item.compareExpr },${item.ratio}`);
+        }
+      })
+    }
+    allValues.filter_options=filter_options.join('@');
+    if (!allValues.filter_options) delete allValues.filter_options;
+    delete allValues.filter_options_list;
     setFilterParams(allValues);
   };
 
@@ -30,6 +40,30 @@ const SearchForm: React.FC<SearchFormProps> = ({ recommendation, setFilterParams
     recommendation.map((r: { workloadType: any }) => ({ value: r.workloadType, label: r.workloadType })),
     'value',
   );
+
+  const resourceOptions = [{
+    label:'cpu',value:'cpu'
+  },{
+    label:'mem',value:'mem'
+  }];
+
+  const quotaTypeOptions = [{
+    label:'limit',value:'limit'
+  },{
+    label:'request',value:'request'
+  }];
+  
+  const compareExprOptions = [{
+    label:'eq(=)',value:'eq'
+  },{
+    label:'gt(>)',value:'gt'
+  },{
+    label:'ge(>=)',value:'ge'
+  },{
+    label:'lt(<)',value:'lt'
+  },{
+    label:'le(<=)',value:'le'
+  }];
 
   return (
     <div className='list-common-table-query'>
@@ -61,6 +95,47 @@ const SearchForm: React.FC<SearchFormProps> = ({ recommendation, setFilterParams
                 </FormItem>
               </Col>
             </Row>
+            <FormList name="filter_options_list">
+              {(fields, {add, remove }) => (
+                <>
+                  {fields.map(({ key, name }) => (
+                    <FormItem key={key}>
+                      <FormItem label={t('资源类型')} name={[name, 'resource']} >
+                        <Select
+                          options={resourceOptions}
+                          placeholder={t('请选择资源类型')}
+                        />
+                      </FormItem>
+                      <FormItem label={t('限制类型')} name={[name, 'quotaType']}>
+                        <Select
+                          options={quotaTypeOptions}
+                          placeholder={t('请选择限制类型')}
+                          style={{ margin: '0px 20px' }}
+                        />
+                      </FormItem>
+                      <FormItem label={t('比较表达式')} name={[name, 'compareExpr']} >
+                        <Select
+                          options={compareExprOptions}
+                          placeholder={t('请选择比较表达式')}
+                          style={{ margin: '0px 20px' }}
+                        />
+                      </FormItem>
+                      <FormItem label={t('比值')} name={[name, 'ratio']}>
+                        <Input placeholder={t('请输入比值')} />
+                      </FormItem>
+                      <FormItem>
+                        <MinusCircleIcon size="20px" style={{ cursor: 'pointer' }} onClick={() => remove(name)} />
+                      </FormItem>
+                    </FormItem>
+                  ))}
+                  <FormItem style={{ marginLeft: 100 }}>
+                    <Button theme="default" variant="dashed" onClick={() => add({})}>
+                      添加过滤条件
+                    </Button>
+                  </FormItem>
+                </>
+              )}
+            </FormList>
           </Col>
           <Col>
             <Button type='reset' variant='base' theme='default'>
