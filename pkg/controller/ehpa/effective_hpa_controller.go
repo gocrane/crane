9ed25e1471aaacea787bd4c71dbb3f3fd7280a38
lbbniu <lbbniu@gmail.com>
2023-04-26 14:32:26 +0800

    fix: update role config

--- pkg/controller/ehpa/effective_hpa_controller.go
+++ pkg/controller/ehpa/effective_hpa_controller.go
@@ -45,7 +45,7 @@ type EffectiveHPAController struct {
 //+kubebuilder:rbac:groups=core,resources=pods;nodes,verbs=get;list;watch
 //+kubebuilder:rbac:groups=core,resources=configmaps,verbs=get;list;watch;update
 //+kubebuilder:rbac:groups=core,resources=events,verbs=create;update;patch
-//+kubebuilder:rbac:groups=apps,resources=deployments;statefulsets,verbs=get;list;watch
+//+kubebuilder:rbac:groups=apps,resources=deployments;deployments/scale;statefulsets;statefulsets/scale,verbs=get;list;watch
 //+kubebuilder:rbac:groups=autoscaling,resources=horizontalpodautoscalers,verbs=*
 //+kubebuilder:rbac:groups=coordination.k8s.io,resources=leases,verbs=get;update
 //+kubebuilder:rbac:groups=analysis.crane.io,resources=*,verbs=*
